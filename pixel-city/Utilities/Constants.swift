a3fe6b3497cb4010cec5ceed610e91aa5848d71a
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-02-14 22:17:00 -0500

    Added urls from API from flickr

--- pixel-city/Utilities/Constants.swift
+++ pixel-city/Utilities/Constants.swift
@@ -11,8 +11,7 @@ import Foundation
 let apiKey = "18f15a4431234c48f74cbc933f23c98d"
 
 func flickrUrl(forApiKey key: String , withAnnotation annotation: DroppablePin, andNumberOfPhotos number:Int) -> String {
-    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude).8&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
-    
+    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
 }
 
 
